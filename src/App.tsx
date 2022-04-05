e6f60b0516f1bf6ef425b64d2e2d6a66c5f555be
DABIN JEONG <41@b41.kr>
2022-04-05 20:12:22 +0900

    Add - 로딩 기능 추가
    - 유저 정보를 받고 로그인 여부 확인 후 로딩에서 라우팅 페이지로 이동
    - 로그인된 경우에는 전역에 isLogin상태를 true로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { authService } from './fbase';
 
+import Loading from 'components/Loading/Loading';
+import Router from 'components/Router/Router';
+import { loginStore } from './zustand/loginStore';
+
 function App() {
-  return <div className="App"></div>;
+  const [init, setInit] = useState<boolean>(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      user && loginStore((state) => state.updateLoginState);
+      setInit(true);
+    });
+  }, []);
+
+  return <div className="App">{init ? <Router /> : <Loading />}</div>;
 }
 
 export default App;
