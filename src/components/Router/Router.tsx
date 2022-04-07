6678452fb66b64dd93ed328085d1de31e39d824c
DABIN JEONG <41@b41.kr>
2022-04-07 15:13:10 +0900

    Add - 라우터 적용
    - 로그아웃 상태에서 로그인 컴포넌트로 이동

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from 'components/Login/Login';
+import Main from 'pages/Main/Main';
+import NotFound from 'pages/NotFound/NotFound';
+import loginStore from 'store/loginStore';
 
 const Router = () => {
-  return <div>Router</div>;
+  const loginState = loginStore((state) => state.isLogin);
+  return (
+    <BrowserRouter basename="/">
+      <Routes>
+        {loginState ? (
+          <Route path="/" element={<Main />} />
+        ) : (
+          <Route path="/*" element={<Login />} />
+        )}
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
